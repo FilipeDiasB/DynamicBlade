81b37e6970f1e41ef20241f42b3c8bd15b5e4b7f
FilipeDiasB <filipe.dbitt@gmail.com>
2022-04-28 14:34:47 -0300

    feat/inputs

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,9 +1,8 @@
-<div>
-    <div class="form-group row">
-        <label for="{{ $input['label'] }}" class="col-md-4 col-form-label text-md-right">{{ $input['label'] }}</label>
-        {{--        <div class="col-md-6">--}}
-        {{--            <input id="" type="text" class="form-control" value="" placeholder="{{ $input['placeholder'] ?? '' }}" autofocus>--}}
-        {{--        </div>--}}
-        {{--    </div>--}}
+<div class="form-group row">
+    <label for="{{ $input['label'] }}" class="col-md-4 col-form-label text-md-right">{{ __($input['label']) }}</label>
+
+    <div class="col-md-6">
+        <input id="{{ $input['name'] }}" type="text" class="form-control @error($input['name']) is-invalid @enderror" name="{{ $input['name'] }}"
+               value="" required autocomplete="{{ $input['name'] }}" autofocus>
     </div>
 </div>
