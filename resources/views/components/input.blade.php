80428f146c1368a87bb7da4addf062811d8c9c58
FilipeDiasB <filipe.dbitt@gmail.com>
2022-04-29 14:37:20 -0300

    Estilo

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group row">
-    <label for="{{ $input['label'] }}" class="col-md-4 col-form-label text-md-right">{{ __($input['label']) }}</label>
+    <label for="{{ $input['label'] }}" class="col-md-4 col-form-label text-md-right"><h4>{{ __($input['label']) }}</h4></label>
 
     <div class="col-md-6">
         <input id="{{ $input['name'] }}" type="text" class="form-control @error($input['name']) is-invalid @enderror" name="{{ $input['name'] }}"
-               value="" required autocomplete="{{ $input['name'] }}" autofocus>
+               value="" required autocomplete="{{ $input['name'] }}" placeholder="{{ $input['placeholder'] }}" autofocus>
     </div>
 </div>
